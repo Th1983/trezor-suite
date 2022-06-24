1e6e7455c9cb7f75f572d119bc811436e165d8ee
juriczech <bohdan.juricek@gmail.com>
2022-06-24 10:26:20 +0200

    feat(suite-native): Create device settings screen

--- suite-native/module-settings/src/components/DeviceSettings.tsx
+++ suite-native/module-settings/src/components/DeviceSettings.tsx
@@ -1,20 +1,37 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
+import { SettingsScreenProp, SettingsStackRoutes } from '../navigation/routes';
 
 import { SettingsSection } from './SettingsSection';
 import { SettingsSectionItem } from './SettingsSectionItem';
 
-export const DeviceSettings = () => (
-    <SettingsSection title="Device">
-        <SettingsSectionItem title="Customization" iconName="palette" subtitle="Name, Homescreen" />
-        <SettingsSectionItem
-            title="Security"
-            iconName="lock"
-            subtitle="PIN Active, Passphrase enabled"
-        />
-        <SettingsSectionItem
-            title="Danger Area"
-            iconName="lock"
-            subtitle="Factory reset, Custom firmware"
-        />
-    </SettingsSection>
-);
+export const DeviceSettings = () => {
+    const navigation = useNavigation<SettingsScreenProp>();
+
+    const handleNavigate = (route: SettingsStackRoutes): void => {
+        navigation.navigate(route);
+    };
+
+    return (
+        <SettingsSection title="Device">
+            <SettingsSectionItem
+                title="Customization"
+                iconName="palette"
+                subtitle="Name, Homescreen"
+                onPress={() => handleNavigate(SettingsStackRoutes.SettingsCustomization)}
+            />
+            <SettingsSectionItem
+                title="Security"
+                iconName="lock"
+                subtitle="PIN Active, Passphrase enabled"
+                onPress={() => handleNavigate(SettingsStackRoutes.SettingsSecurity)}
+            />
+            <SettingsSectionItem
+                title="Danger Area"
+                iconName="lock"
+                subtitle="Factory reset, Custom firmware"
+                onPress={() => handleNavigate(SettingsStackRoutes.SettingsDangerArea)}
+            />
+        </SettingsSection>
+    );
+};
